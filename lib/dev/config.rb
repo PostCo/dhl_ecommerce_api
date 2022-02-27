73f55973998c02a1c8aa7c6970bd769bd53ad956
Er Whey <erwhey@gmail.com>
2022-02-27 15:19:30 +0800

    add pickup and soldto config variables

--- lib/dev/config.rb
+++ lib/dev/config.rb
@@ -5,5 +5,7 @@ def set_config
     config.env = :sandbox
     config.client_id = ENV["DHL_ECOMMERCE_API_CLIENT_ID"]
     config.password = ENV["DHL_ECOMMERCE_API_PASSWORD"]
+    config.pickup_account_id = ENV["DHL_ECOMMERCE_API_PICKUP_ACCOUNT_ID"]
+    config.soldto_account_id = ENV["DHL_ECOMMERCE_API_SOLDTO_ACCOUNT_ID"]
   end
 end
