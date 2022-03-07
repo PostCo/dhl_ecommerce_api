6b791fc5b3f56f213610ea515c5cedabafd8ea98
Er Whey <erwhey@gmail.com>
2022-03-07 11:12:01 +0800

    rename shipment_creation to shipment

--- lib/dhl_ecommerce_api/resources/shipment/shipment_item.rb
+++ lib/dhl_ecommerce_api/resources/shipment/shipment_item.rb
@@ -1,11 +1,10 @@
 module DHLEcommerceAPI
-  class ShipmentCreation::ShipmentItem < Base
+  class Shipment::ShipmentItem < Base
     def initialize(attributes = {}, persisted = false)
       status = attributes["response_status"]
       if status.present? && status["code"] == "200"
           error_messages = status["message_details"].map{|err| err["message_detail"]}
           handle_errors(status["code"], error_messages) 
-        end
       end
       super
     end
