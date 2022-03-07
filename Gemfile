6e452adc6e79552c65220049a7bd1d7f974d6b3d
Er Whey <erwhey@gmail.com>
2022-03-07 09:49:36 +0800

    rspec auth

--- Gemfile
+++ Gemfile
@@ -8,5 +8,5 @@ gemspec
 gem "rake", "~> 13.0"
 
 gem "rspec", "~> 3.0"
-
+gem "webmock"
 gem "rubocop", "~> 1.21"
--- spec/dhl_ecommerce_api_spec.rb
+++ spec/dhl_ecommerce_api_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe DHLEcommerceAPI do
   it "has a version number" do
     expect(DHLEcommerceAPI::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
